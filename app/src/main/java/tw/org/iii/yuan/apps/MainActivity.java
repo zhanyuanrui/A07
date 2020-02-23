31e2bdeecbab835f940ce5f4d894e31165f3b5af
user <a0903501246@gmail.com>
2020-02-23 16:05:03 +0800

    20200223OK

--- app/src/main/java/tw/org/iii/yuan/apps/MainActivity.java
+++ app/src/main/java/tw/org/iii/yuan/apps/MainActivity.java
@@ -3,12 +3,29 @@ package tw.org.iii.yuan.apps;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.HashSet;
 
 public class MainActivity extends AppCompatActivity {
-
+private TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv = findViewById(R.id.textView);
+    }
+
+    public void click(View view) {
+        HashSet<Integer> set = new HashSet<>();
+        while (set.size()<6){
+            set.add((int)(Math.random()*49+1));
+
+        }
+        //Log.v("yuan" , set.toString());
+        tv.setText(set.toString());
     }
 }
